b5a04a83b7c4b1f79f3d56d1c4d32f35f0e1b3be
matthew-woodington <smwoodington@gmail.com>
2023-04-05 20:31:34 -0400

    import post page

--- frontend/pages/post/[slug].tsx
+++ frontend/pages/post/[slug].tsx
@@ -1,15 +1,72 @@
+import groq from "groq"
 import client from "../../client"
+import imageUrlBuilder from "@sanity/image-url"
+import { PortableText } from "@portabletext/react"
+import { format } from "path"
 
-const Post = ({ post } : {post:any}) => {
+function urlFor (source:string) {
+  return imageUrlBuilder(client).image(source)
+}
 
-  console.log(post)
+const ptComponents = {
+  types: {
+    image: ({ value } : {value:any}) => {
+      if (!value?.asset?._ref) {
+        return null
+      }
+      return (
+        <picture>
+          <img
+            alt={value.alt || ' '}
+            loading="lazy"
+            src={urlFor(value).width(320).height(240).fit('max').auto('format')}
+          />
+        </picture>
+      )
+    }
+  }
+}
+
+const Post = ({post} : {post:any}) => {
+
+  const { 
+    title = 'Missing title', 
+    name = 'Missing name', 
+    categories, 
+    authorImage,
+    body = []
+  } = post
 
   return (
     <article>
-      <h1>{post?.title}</h1>
+      <h1>{title}</h1>
+      <span>By {name}</span>
+      {categories && (
+        <ul>
+          Posted in:
+          {categories.map((category:string) => <li key={category}>{category}</li>)}
+        </ul>
+      )}
+      {authorImage && (
+        <picture>
+          <img src={urlFor(authorImage).width(50).url()} alt={`${name}'s picture`} />
+        </picture>
+      )}
+      <PortableText 
+        value={body}
+        components={ptComponents}
+      />
     </article>
   )
 }
+
+const query = groq`*[_type == "post" && slug.current == $slug][0]{
+  title,
+  "name": author->name,
+  "categories": categories[]->title,
+  "authorImage": author->image,
+  body
+}`
 
 export async function getStaticPaths() {
   const paths = await client.fetch(
@@ -24,7 +81,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context:any) {
   const { slug = '' } = context.params
-  const post = await client.fetch(`*[_type == "post" && slug.current == $slug][0]`, { slug })
+  const post = await client.fetch(query, { slug })
 
   return {
     props: {
