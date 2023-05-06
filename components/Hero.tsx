78bd5ff64eaa9aba6c20ed748faa7c4b1cbf6362
matthew-woodington <smwoodington@gmail.com>
2023-05-06 11:49:22 -0400

    hero comp

---
+++ components/Hero.tsx
@@ -0,0 +1,21 @@
+import styles from '../styles/Hero.module.css'
+import HeroImage from '../assets/images/heroimage.png'
+
+function Hero() {
+  return (
+    <section className={styles.herosection}>
+      <picture>
+        <img className={styles.heroimg} src={HeroImage.src} alt="couch image" />
+      </picture>
+      <div>
+        <h1 className={styles.herotitle}>
+          Musings from a<br></br>
+          Double sided<br></br>
+          Couch
+        </h1>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
