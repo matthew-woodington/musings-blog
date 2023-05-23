ec753db4aefc473f66dfdcc0f41987017196f390
matthew-woodington <smwoodington@gmail.com>
2023-05-23 17:05:12 -0400

    footer

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "426e925554640c99"; }
+/******/ 		__webpack_require__.h = function() { return "5404c945cd27f0dd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,18 @@
+import styles from '../styles/Footer.module.css'
+
 function Footer() {
   return (
-    <footer>
-      <section className="footer-title">
-        <h1>
+    <footer className={styles.footer}>
+      <section>
+        <h1 className={styles.footertitle}>
           Musings from a<br></br>
           Double sided<br></br>
           Couch
         </h1>
+      </section>
+      <section className={styles.closersection}>
+        <p className={styles.closer}>©musingsfromadoublesidedcouch2023</p>
+        <p className={styles.closer}>All Rights Reserved</p>
       </section>
     </footer>
   )
